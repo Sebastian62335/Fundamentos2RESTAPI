2057c687fb990e66d3728ac52fc9a57cb524496c
sebas62335 <sebastian62335@hotmail.com>
2022-08-11 15:34:06 -0500

    with postman

---
+++ src/test/java/com/platzi/springboot/Fundamentos2Tests.java
@@ -0,0 +1,13 @@
+package com.platzi.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fundamentos2Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
